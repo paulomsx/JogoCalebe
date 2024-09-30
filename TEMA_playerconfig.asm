d1487fd13ff1a523859b7bfd6c03b98f99d3823e
paulomsx <paulo.bola2@gmail.com>
2024-09-29 23:44:38 -0300

    Iniciando o projeto

---
+++ TEMA_playerconfig.asm
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+	PLY_CFG_ConfigurationIsPresent = 1
+	PLY_CFG_UseSpeedTracks = 1
+	PLY_CFG_UseHardwareSounds = 1
+	PLY_CFG_UseEffects = 1
+	PLY_CFG_UseInstrumentLoopTo = 1
+	PLY_CFG_NoSoftNoHard = 1
+	PLY_CFG_NoSoftNoHard_Noise = 1
+	PLY_CFG_SoftOnly = 1
+	PLY_CFG_SoftOnly_Noise = 1
+	PLY_CFG_SoftOnly_SoftwarePitch = 1
+	PLY_CFG_HardOnly = 1
+	PLY_CFG_UseEffect_PitchUp = 1
+	PLY_CFG_UseEffect_PitchDown = 1
+	PLY_CFG_UseEffect_ArpeggioTable = 1
+	PLY_CFG_UseEffect_SetVolume = 1
